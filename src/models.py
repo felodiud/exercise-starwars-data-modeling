45ac2916792987c0bfc85dbac2e253b27ea044ba
felodiud <127905243+felodiud@users.noreply.github.com>
2023-07-19 23:53:37 +0000

    finished tables

--- src/models.py
+++ src/models.py
@@ -1,29 +1,70 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), nullable=False)
+    name = Column(String(250))
+    lastname = Column(String())
+    origin_planet = Column(String())
+    favorite = relationship('Favorite')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+# class Ship
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    ship_id = Column(Integer, ForeignKey('ship.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    ship = relationship('Ship')
+    planet = relationship('Planet')
+    character = relationship('Character')
+
+class Ship(Base):
+    __tablename__ = 'ship'
+    id = Column(Integer, primary_key = True)
+    model = Column(String(400), nullable = False)
+    wheigth = Column(String(400), nullable = False)
+    hp = Column(Integer, nullable = False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(400), nullable = False)
+    habitats = Column(String(400), nullable = False)
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key = True)
+    fullname = Column(String(400), nullable = False)
+    occupation = Column(String(400), nullable = False)
+    powers = Column(Boolean, nullable = False)
+    origin_planet = Column(String(400), nullable = False)
+
+
+
 
     def to_dict(self):
         return {}
